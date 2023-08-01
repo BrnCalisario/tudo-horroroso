3b6015afed2d90bfdd98220b644ce21749f4f221
LucaFreire <lucasrodifreire@gmail.com>
2023-08-01 16:48:24 -0300

    add: homePage

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    title: 'homePage',
+    component: HomePageComponent
+  },
   {
     path: '**',
     title: "Error",
