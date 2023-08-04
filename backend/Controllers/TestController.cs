6d0d3d2c9d6e99b6c33342725e5efb4c21db9169
BrnCalisario <bruno145224@gmail.com>
2023-08-04 14:27:32 -0300

    init: backend

---
+++ backend/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TudoHorroroso.Controllers;
+
+
+[ApiController]
+[EnableCors("MainPolicy")]
+[Route("test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Get()
+        => Ok();
+
+}
---
+++ backend/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MainPolicy",
+    policy =>
+    {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyOrigin()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
